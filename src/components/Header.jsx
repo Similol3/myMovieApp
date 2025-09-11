ea9cfa5af1a2724af3bc6ca3ae0d2fc2a875735f
Similol3 <matthewotusajo@gmail.com>
2025-09-11 04:33:49 -0700

    added fixed to header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 export default function Header() {
   return (
-    <header className="flex justify-between p-6 shadow-md items-center ">
-      <h2 className="font-bold text-xl">myFoodRecipe</h2>
+    <div className="w-full">
+    <header className="fixed w-full flex justify-between p-6 shadow-md items-center bg-white z-50">
+      <h2 className="font-bold text-xl ">myFoodRecipe</h2>
       <button className="flex flex-col gap-1">
         <span className="bg-black w-4 h-1 inline-block"></span>
         <span className="bg-black w-4 h-1 inline-block"></span>
         <span className="bg-black w-4 h-1 inline-block"></span>
       </button>
     </header>
+    </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,10 +2,10 @@ import { Search } from "lucide-react";
 
 export default function Searchbar() {
   return (
-    <div className="flex p-5 gap-2 items-center ">
+    <div className="flex p-5 items-center gap-6 ">
       <input
         type="text"
-        className="border w-full outline-none p-2 rounded border-gray-500"
+        className="border w-full outline-none p-2 rounded border-gray-500 my-[18%]"
       />
       <Search />
     </div>
